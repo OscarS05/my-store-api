11ab91beb45edc7cb5ae45325697c1e987f6f218
OscarS05 <santiagom0509@hotmail.com>
2025-05-08 11:50:22 -0500

    start of tests

---
+++ e2e/app.e2e.js
@@ -0,0 +1,27 @@
+const request = require('supertest');
+
+const createApp = require('../src/app');
+
+describe('tests for app', () => {
+  let app = null;
+  let server = null;
+  let api = null;
+
+  beforeEach(() => {
+    app = createApp();
+    server = app.listen(9000);
+    api = request(app);
+  });
+
+  test('GET /hello', async () => {
+    const response = await api.get('/hello');
+    expect(response).toBeTruthy();
+    expect(response.statusCode).toEqual(200);
+    expect(response.body.name).toEqual('oscar');
+    expect(response.headers['content-type']).toMatch(/json/);
+  });
+
+  afterEach(() => {
+    server.close(); //Cerrar el servidor es una buena práctica
+  });
+});
